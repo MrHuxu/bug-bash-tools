6c200f980b8b147f21eac98b123af74ad26a250a
xhu <hxtheone@gmail.com>
2016-07-27 23:16:14 +0800

    make the delete work

--- go-server/server.go
+++ go-server/server.go
@@ -35,28 +35,7 @@ func createBucket(db *bolt.DB, bucketName string) {
 	})
 }
 
-func createBugBash(db *bolt.DB, bb *bugBash) {
-	db.Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte("bug-bash"))
-
-		id, err := bucket.NextSequence()
-		bb.ID = int(id)
-		buf, err := json.Marshal(bb)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return bucket.Put([]byte(itob(bb.ID)), buf)
-	})
-}
-
-func itob(v int) []byte {
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, uint64(v))
-	return b
-}
-
-func findAndRenderBugBash(db *bolt.DB, r render.Render) {
+func findBugBashAndRender(db *bolt.DB, r render.Render) {
 	db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte("bug-bash"))
 		cursor := bucket.Cursor()
@@ -72,6 +51,42 @@ func findAndRenderBugBash(db *bolt.DB, r render.Render) {
 	})
 }
 
+func createBugBashAndRender(db *bolt.DB, bb *bugBash, r render.Render) {
+	db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("bug-bash"))
+
+		id, err := bucket.NextSequence()
+		bb.ID = int(id)
+		buf, err := json.Marshal(bb)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		r.JSON(200, map[string]*bugBash{"record": bb})
+		return bucket.Put([]byte(itob(bb.ID)), buf)
+	})
+}
+
+func destroyBugBashAndRender(db *bolt.DB, bb *bugBash, r render.Render) {
+	db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("bug-bash"))
+		log.Println("======", bb)
+		err := bucket.Delete(itob(bb.ID))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		r.JSON(200, map[string]*bugBash{"record": bb})
+		return err
+	})
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
 func main() {
 	server := martini.Classic()
 	server.Use(render.Renderer())
@@ -83,25 +98,31 @@ func main() {
 	// implement bug-bash series routers
 	server.Group("/bug-bash", func(r martini.Router) {
 		r.Get("/", func(req *http.Request, r render.Render) {
-			findAndRenderBugBash(db, r)
+			findBugBashAndRender(db, r)
 		})
 
-		r.Post("/new", func(req *http.Request) {
+		r.Post("/new", func(req *http.Request, r render.Render) {
 			decoder := json.NewDecoder(req.Body)
 			var bb bugBash
 			err := decoder.Decode(&bb)
 			if err != nil {
 				log.Fatal(err)
 			}
-			createBugBash(db, &bb)
+			createBugBashAndRender(db, &bb, r)
 		})
 
 		r.Put("/update", func(req *http.Request) {
 			log.Println(req)
 		})
 
-		r.Delete("/destroy", func(req *http.Request) {
-			log.Println(req)
+		r.Delete("/destroy", func(req *http.Request, r render.Render) {
+			decoder := json.NewDecoder(req.Body)
+			var bb bugBash
+			err := decoder.Decode(&bb)
+			if err != nil {
+				log.Fatal(err)
+			}
+			destroyBugBashAndRender(db, &bb, r)
 		})
 	})
 
